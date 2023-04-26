f682a69c84fc43e5e7beef84a075d21ac69c6ed3
Nwaigba66 <glorianwaigba@gmail.com>
2023-04-26 15:01:05 -0700

    Simple shell commit

---
+++ path.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+char *get_path(char *command) {
+    char *path = getenv("PATH");
+    char *dir;
+    char *path_copy = strdup(path);
+
+    for (dir = strtok(path_copy, ":"); dir; dir = strtok(NULL, ":")) {
+        char *full_path = malloc(strlen(dir) + strlen(command) + 2);
+        sprintf(full_path, "%s/%s", dir, command);
+        if (access(full_path, X_OK) == 0) {
+            free(path_copy);
+            return full_path;
+        }
+        free(full_path);
+    }
+    free(path_copy);
+    return NULL;
+}
+
--- simple_shell.h
+++ simple_shell.h
@@ -1,19 +1,20 @@
 #ifndef SIMPLE_SHELL_H
 #define SIMPLE_SHELL_H
 
-/* Standard Libraries */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <errno.h>
 
-/* Function Prototypes */
-char *read_line(void)
-char **split_line(char *line)
-int main(void)
-char *getline(void)
+char *_strdup(char *str);
+int _strlen(char *s);
+char **tokenize(char *str, char *delimiter);
+char *get_path(char *command);
+char **get_all_paths(void);
+int execute(char **arguments);
+int shell_loop(void);
+int main(void);
+#endif
 
-#endif 
